515fb3afcbcc0bb82011389e4daf57c1e05a1b70
majou <42233-majou@users.noreply.gitgud.io>
2024-06-17 08:41:25 +0000

    fix bad conditions

--- src/ui.js
+++ src/ui.js
@@ -481,7 +481,7 @@ var UI = (() => { // eslint-disable-line no-unused-vars, no-var
 				'idbToggleSaves',
 				null,
 				'Enable indexedDB',
-				Save.autosave.has() || !Save.slots.isEmpty()
+				!idb.lock
 					? () => {
 						idb.updateSettings('active', true);
 						idb.saveList();
